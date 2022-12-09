de668bfce1c2d2329d7073bead23dc40a2404dd4
Ansidion <andrew.webster403@hotmail.co.uk>
2022-12-09 21:14:33 +0000

    latest changes

--- dbconnect.php
+++ dbconnect.php
@@ -1,18 +1,4 @@
 <?php
-// This is the version you would use ordinarily...
-
-// the OK and NotOK versions put out messages to screen which screw things up 
-// when you do try to use the database for real...
-
-
-// Sets up the connection parameters to a mysql database
-
-// To use you edit the parameters into the '' below
-
-// If connection fails it's usually one of the parameters has been mistyped
-// or the server's down...
-
-
 DEFINE ('DB_USER', 'inhnc');                           // The username
 DEFINE ('DB_PASSWORD','inhnc');                        // The password
 DEFINE ('DB_HOST', 'localhost');                       // The mysql server host address 
