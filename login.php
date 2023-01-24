d075aa39480a8fdfbb874dfa59f297a8864f598e
Ansidion <andrew.webster403@hotmail.co.uk>
2023-01-24 16:29:46 +0000

    correcting fields

--- login.php
+++ login.php
@@ -1,23 +1,26 @@
 <?php require 'header.php' ?>
 
-        <h2>Theatre User Login</h2>
-        <h2>Please enter your Email address and password</h2>
+<h2>Theatre User Login</h2>
+<h2>Please enter your Email address and password</h2>
 
-        <form id="loginForm" method="post" action="authenticate.php">
-            <table>
-                <tr>
-                    <td>Email address :</td>
-                    <td><input type="text" name="Email" size="30" /></td>
-                </tr>
-                <tr>
-                    <td>Password:</td>
-                    <td><input type="password" name="Password" size="10" /></td>
-                </tr>
+<form id="loginForm" method="post" action="authenticate.php">
+    <table>
+        <tr>
+            <td>Email</td>
+            <td><input type="text" name="email" size="10" /></td>
+        </tr>
+        <tr>
+            <td>Username</td>
+            <td><input type="text" name="username" size="10" /></td>
+        </tr>
+        <tr>
+            <td>Password</td>
+            <td><input type="password" name="password" size="10" /></td>
+        </tr>
+        <tr>
+            <td colspan="2"><input type="submit" value="Check User" /></td>
+        </tr>
+    </table>
+</form>
 
-                <tr>
-                    <td colspan="2"><input type="submit" value="Check User" /></td>
-                </tr>
-            </table>
-        </form>
-
-    <?php require 'footer.php' ?>
+<?php require 'footer.php' ?>
