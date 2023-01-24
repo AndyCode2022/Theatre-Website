8ce3d45daa81dde8d578761711aaf84a450ac3f3
Ansidion <andrew.webster403@hotmail.co.uk>
2023-01-24 16:30:26 +0000

    correcting update users

--- processUpdateUser.php
+++ processUpdateUser.php
@@ -1,20 +1,23 @@
-<?php
-
-require 'header.php';
-
-?>
+<?php require 'header.php'; ?>
 
 <?php
 
 require "checkLogin.php";
 require "dbconnect.php";
 
-$customerno = $_SESSION['customerno'];
+$customerno = $_SESSION['customerNo'];
 
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$address = $_POST['address'];
+$town = $_POST['town'];
+$postcode = $_POST['postcode'];
 $email = $_POST['email'];
 
-$sql = "UPDATE customers SET email = '$email'
-        WHERE customerno = $customerno ";
+$sql = "UPDATE users SET firstname = '$firstname' , lastname = '$lastname' , address = '$address' ,
+        town = '$town' , postcode = '$postcode' , email = '$email'
+        WHERE userno = $userno ";
+
 
 if ($conn->query($sql) == true) {
     echo "<p>Thanks your info has been updated.</p>";
