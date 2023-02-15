c7278bbf9b3cda4c6182918820142e4f6f1efd8c
Ansidion <14023216@uhi.ac.uk>
2023-02-15 16:59:17 +0000

    took out nav bar bootstrap

--- footer.php
+++ footer.php
@@ -4,11 +4,11 @@
 
 <!-- Custom styles for this template -->
 
-    <footer class="footer mt-auto py-3 bg-light">
-        <div class="container">
-            <span class="text-muted">Place sticky footer content here.</span>
-        </div>
-    </footer>
+<footer class="footer mt-auto py-3 bg-light">
+    <div class="container">
+        <span class="text-muted">Theatre</span>
+    </div>
+</footer>
 </body>
 
 </html>
--- header.php
+++ header.php
@@ -36,46 +36,12 @@ setcookie("test_cookie", "test", time() + 3600, '/');
             <li><a href="microBlog.php">MicroBlog</a></li>
             <li><a href="register.php">Register</a></li>
             <li><a href="Login.php">Login</a></li>
-            <div class="button1">
-                <button onclick="functionDark()">Dark Mode</button>
-            </div>
         </ul>
     </nav>
-
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled">Disabled</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    
+    <div class="button1">
+        <button onclick="functionDark()">Dark Mode</button>
+    </div>
 
     <h1>Welcome To The Theatre Website</h1>
 
