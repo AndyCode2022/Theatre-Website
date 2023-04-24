855aee09b656648957de97398abb927fa60643b2
Ansidion <14023216@uhi.ac.uk>
2023-04-24 22:07:02 +0100

    removing unnecessary semi colons

--- includes/displayBlogs.php
+++ includes/displayBlogs.php
@@ -8,14 +8,14 @@ $postResult = mysqli_query($conn, $sql);
 if (mysqli_num_rows($postResult) > 0) {
     while ($postText = mysqli_fetch_assoc($postResult)) {
         // Displays the posts on the page
-        echo '<div class="container">;
-              <div class="card mb-3">;
-              <div class="card-body">;
-              <h5 class="card-title"> . htmlspecialchars($postText["title"]) . </h5>;
-              <div class="card mb-3">;
-              <input name="post_text" value="' . $postText['postText'] . '">;
+        echo '<div class="container">
+              <div class="card mb-3">
+              <div class="card-body">
+              <h5 class="card-title">' . htmlspecialchars($postText["title"]) . '</h5>
+              <div class="card mb-3">
+              <input name="post_text" value="' . $postText['postText'] . '">
               </div>';
-        echo '<p class="card-text">Posted by user ' . ($postText['postText']) . ' on ' . date('d-m-Y', strtotime($postText['date_created'])) . '</p>';
+        echo '<p class="card-text">Posted by user ' . ($postText['userno']) . ' on ' . date('d-m-Y', strtotime($postText['date_created'])) . '</p>';
 
         // print_r($postText);
         if (!empty($row) && isset($row['post_ID']))
@@ -29,11 +29,11 @@ if (mysqli_num_rows($postResult) > 0) {
                 // Displayed comments
                 echo '<div class="card-body">';
                 if (!empty($row) && isset($row['comment']))
-                echo '<p class="card-text"> ' . htmlspecialchars($comment['comment']) . '</p>;
-                      </div>;
-                      <div class="card-footer">;
-                      <small class="text-muted"> ' . ($comment["date_created"]) . '</small>;
-                      </div>;
+                echo '<p class="card-text"> ' . htmlspecialchars($comment['comment']) . '</p>
+                      </div>
+                      <div class="card-footer">
+                      <small class="text-muted"> ' . ($comment["date_created"]) . '</small>
+                      </div>
                       </div>';
             }
         } else {
@@ -41,14 +41,14 @@ if (mysqli_num_rows($postResult) > 0) {
         }
 
         // Comment input for user
-        echo '<div class="container">';
-        '<div class="card-body">;
-        <form method="post" action="processNewComment.php>;
-        <input type="hidden" name="post_id" value="' . isset($postText['post_ID']) . '">;
-        <textarea class="form-control" id="comment" name="comment" rows="10" required="yes">Add your comment to the post</textarea><br><br>;
-        <input class="form-control" type="submit" value="Submit">;
-        </form>;
-        </div>;
+        echo '<div class="container">
+              <div class="card-body">
+              <form method="post" action="processNewComment.php>
+              <input type="hidden" name="post_id" value="' . isset($postText['post_ID']) . '">
+              <textarea class="form-control" id="comment" name="comment" rows="10" required="yes">Add your comment to the post</textarea><br><br>
+              <input class="form-control" type="submit" value="Submit">
+            </form>
+          </div>
         </div>';
     }
     // Displayed comments with edit functionality
@@ -59,28 +59,27 @@ if (mysqli_num_rows($postResult) > 0) {
     if (mysqli_num_rows($commentResult) > 0) {
         while ($comment = mysqli_fetch_assoc($commentResult)) {
             // Edit functionality
-            echo '<div class="container">;
-                  <div class="card-body">;
-                  <form method="post" action="serverFiles/editComment.php">;
+            echo '<div class="container">
+                  <div class="card-body">
+                  <form method="post" action="serverFiles/editComment.php">
                   <input type="hidden" name="comment_id" value="' . ($comment['userno']) . '">';
             if (!empty($row) && isset($row['comment']))
-            echo '<input class="form-control" type="text" name="comment_text" value="' . htmlspecialchars($comment['comment']) . '">;
-                  <input class="form-control" type="submit" value="Edit">;
-                  </form>;
-                  </div>;
+            echo '<input class="form-control" type="text" name="comment_text" value="' . htmlspecialchars($comment['comment']) . '">
+                  <input class="form-control" type="submit" value="Edit">
+                  </div>
                   </div>';
             // Delete functionality
-            echo '<div class="container">;
-            <form method="post" action="serverFiles/deleteComment.php">;
+            echo '<div class="container">
+            <form method="post" action="includes/deleteComment.php">
             <input type="hidden" name="comment_id" value="' . isset($userno['userno']) . '">';
             if (!empty($row) && isset($row['body']))
-            echo '<input class="form-control" type="text" name="comment_text" value="' . $body['body'] . '">;
-                  <input class="form-control" type="submit" value="delete">;
+            echo '<input class="form-control" type="text" name="comment_text" value="' . $body['body'] . '">
+                  <input class="form-control" type="submit" value="delete">
                   </div>';
         }
     }
-    echo '</div>;
-          </div>;
+    echo '</div>
+          </div>
           </div>';
 } else {
     echo 'No posts yet';
