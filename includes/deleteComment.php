c1b972dda35856e3fb589f9ad23c68248ba6c019
Ansidion <14023216@uhi.ac.uk>
2023-05-01 14:18:29 +0100

    new changes

--- includes/deleteComment.php
+++ includes/deleteComment.php
@@ -7,19 +7,22 @@ $userno = $_SESSION['userno'];
 $commentno = $_POST['commentno'];
 
 // Query the database to get the userno of the comment owner
-$query = "SELECT userno FROM comments WHERE commentno = $commentno";
+$query = "SELECT commentno FROM comments WHERE commentno = $commentno";
 $result = mysqli_query($connection, $query);
 $row = mysqli_fetch_assoc($result);
 $comment_owner = $row['userno'];
 
 // Check if the logged in user matches the comment owner
 if ($userno == $comment_owner) {
-// Delete the comment from the database
-$query = "DELETE FROM comments WHERE commentno = $commentno";
-mysqli_query($conn, $query);
-// Redirect the user to the posts page
-header("Location: ../microBlog.php");
+
+    // Delete the comment from the database
+    $query = "DELETE FROM comments WHERE commentno = $commentno";
+    mysqli_query($conn, $query);
+
+    // Redirect the user to the posts page
+    header("Location: ../microBlog.php");
 } else {
-// Display an error message
-echo "You do not have permission to delete this comment.";
+
+    // Display an error message
+    echo "You do not have permission to delete this comment.";
 }
--- includes/processNewComment.php
+++ includes/processNewComment.php
@@ -5,18 +5,15 @@ require_once ('dbconnect.php');
 // Submit the comments to the database
 $commentText = isset($_POST['commentText']);
 
-// $query = "INSERT INTO comments (title, commentText) 
-//     VALUES ('$commentText')";
-// mysqli_query($conn, $query);
-
 $stmt = $conn->prepare("SELECT userno FROM users WHERE userno = ?");
 $stmt->bind_param("s", $commentID);
 $stmt->execute();
 $result = $stmt->get_result();
 
 $sql = "INSERT INTO comments (commentText) VALUES (?)";
-
-
+$stmt = mysqli_prepare($conn, $sql);
+$stmt->bind_param("s", $commentText);
+$stmt->execute();
 
 // Close the prepared statement
 if (isset($stmt)) {
